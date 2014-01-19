5d15e89b3186c73afb75176418e4fd6b880c7126
tim.sinaeve@gmail.com <tim.sinaeve@gmail.com@07beed8e-bd92-d2dd-6a11-04ca0414efdf>
2014-01-19 15:52:45 +0000

    * Changed build mode names

--- projects/notepas/build.sh
+++ projects/notepas/build.sh
@@ -1,10 +1,10 @@
 # simple buildscript to make release builds for all supported platforms
 rm lib -r
-typhonbuild --bm="Release win64 win32" notepas.lpr -B
-typhonbuild --bm="Release win32 win32" notepas.lpr -B
-typhonbuild --bm="Release win32 qt" notepas.lpr -B
-typhonbuild --bm="Release win32 gtk2" notepas.lpr -B
-typhonbuild --bm="Release linux32 gtk2" notepas.lpr -B
-typhonbuild --bm="Release linux64 gtk2" notepas.lpr -B
-typhonbuild --bm="Release linux64 qt" notepas.lpr -B
+typhonbuild --bm="Release win64 x86_64 win32" notepas.lpr -B
+typhonbuild --bm="Release win32 i386 win32" notepas.lpr -B
+typhonbuild --bm="Release win32 i386 qt" notepas.lpr -B
+typhonbuild --bm="Release win32 i386 gtk2" notepas.lpr -B
+typhonbuild --bm="Release linux i386 gtk2" notepas.lpr -B
+typhonbuild --bm="Release linux x86_64 gtk2" notepas.lpr -B
+typhonbuild --bm="Release linux x86_64 qt" notepas.lpr -B
 

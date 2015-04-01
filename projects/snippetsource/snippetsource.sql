ccc63f1be3c09ad8291102b57c677dea5fe5a48b
Tim Sinaeve <tim.sinaeve@gmail.com>
2015-04-01 13:52:28 +0200

    * Added missing files

---
+++ projects/snippetsource/snippetsource.sql
@@ -0,0 +1,119 @@
+CREATE TABLE SnippetMetadata (
+    ID            INTEGER         PRIMARY KEY AUTOINCREMENT,
+    HighlighterID INTEGER,
+    NodeTypeID    INTEGER,
+    ParentID      INTEGER,
+    ImageIndex    INTEGER,
+    NodeName      VARCHAR( 255 ),
+    NodePath      TEXT,
+    FoldLevel     INTEGER,
+    CommentRTF    TEXT,
+    FoldState     VARCHAR( 255 ),
+    DateCreated   DATETIME,
+    DateModified  DATETIME
+);
+
+CREATE INDEX idx_SnippetMetadataParentID ON SnippetMetadata (
+    ParentID ASC
+);
+
+CREATE INDEX idx_SnippetMetadataNodePath ON SnippetMetadata (
+    NodePath ASC
+);
+
+CREATE INDEX idx_SnippetMetadataNodeName ON SnippetMetadata (
+    NodeName DESC
+);
+
+CREATE VIRTUAL TABLE SnippetData USING FTS4(
+    Text,
+    Comment
+);
+
+CREATE VIEW Snippet AS
+  SELECT
+    sm.ID,
+    sm.HighlighterID,
+    sm.NodeTypeID,
+    sm.ParentID,
+    sm.ImageIndex,
+    sm.NodeName,
+    sm.NodePath,
+    sm.FoldLevel,
+    sm.CommentRTF,
+    sm.FoldState,
+    sm.DateCreated,
+    sm.DateModified,
+    sd.Text,
+    sd.Comment
+  FROM
+    SnippetMetadata sm
+    INNER JOIN SnippetData sd
+      ON (sm.RowID = sd.RowID);
+
+CREATE TRIGGER Snippet_insert INSTEAD OF INSERT ON Snippet
+BEGIN
+  INSERT INTO SnippetMetadata(
+    ID,
+    HighlighterID,
+    NodeTypeID,
+    ParentID,
+    ImageIndex,
+    NodeName,
+    NodePath,
+    FoldLevel,
+    CommentRTF,
+    FoldState,
+    DateCreated,
+    DateModified
+  ) VALUES (
+    NEW.ID,
+    NEW.HighlighterID,
+    NEW.NodeTypeID,
+    NEW.ParentID,
+    NEW.ImageIndex,
+    NEW.NodeName,
+    NEW.NodePath,
+    NEW.FoldLevel,
+    NEW.CommentRTF,
+    NEW.FoldState,
+    NEW.DateCreated,
+    NEW.DateModified
+  );
+  INSERT INTO SnippetData(
+    RowID,
+    Text,
+    Comment
+  ) VALUES (
+    last_insert_rowid(),
+    NEW.Text,
+    NEW.Comment
+  );
+END;
+
+CREATE TRIGGER Snippet_delete INSTEAD OF DELETE ON Snippet
+BEGIN
+  DELETE FROM SnippetMetadata WHERE RowID = OLD.RowID;
+  DELETE FROM SnippetData WHERE RowID = OLD.RowID;
+END;
+
+
+/*
+CREATE TABLE metadata (document INTEGER, page INTEGER, UNIQUE(document, page));
+CREATE VIRTUAL TABLE data USING fts4();
+
+CREATE VIEW whole AS SELECT metadata.rowid AS rowid, document, page, content
+    FROM metadata JOIN data ON metadata.rowid = data.rowid;
+
+CREATE TRIGGER whole_insert INSTEAD OF INSERT ON whole
+BEGIN
+  INSERT INTO metadata (document, page) VALUES (NEW.document, NEW.page);
+  INSERT INTO data (rowid, content) VALUES (last_insert_rowid(), NEW.content);
+END;
+
+CREATE TRIGGER whole_delete INSTEAD OF DELETE ON whole
+BEGIN
+  DELETE FROM metadata WHERE rowid = OLD.rowid;
+  DELETE FROM data WHERE rowid = OLD.rowid;
+END;
+*/

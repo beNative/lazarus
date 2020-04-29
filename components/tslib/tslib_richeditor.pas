30b49ab9110d7f7e179c5b7663986119f59e311c
Tim Sinaeve <tim.sinaeve@gmail.com>
2020-04-29 10:52:10 +0200

    Added new tslib_richeditor package. Removed unused dependencies.

---
+++ components/tslib/tslib_richeditor.pas
@@ -0,0 +1,24 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit tslib_richeditor;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  ts.RichEditor.Events, ts.RichEditor.Factories.Menus, 
+  ts.RichEditor.Factories, ts.RichEditor.Factories.Toolbars, 
+  ts.RichEditor.Interfaces, ts.RichEditor.Manager, ts.RichEditor.Settings, 
+  ts.RichEditor.Types, ts.RichEditor.View.KMemo, LazarusPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+end;
+
+initialization
+  RegisterPackage('tslib_richeditor', @Register);
+end.
